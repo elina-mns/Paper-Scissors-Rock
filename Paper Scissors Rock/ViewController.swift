a8082784a47577225514f67452cadffaf39236e7
elina-mns <ellie.mansurova@gmail.com>
2020-08-31 11:47:52 -0400

    Initial Commit

---
+++ Paper Scissors Rock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Paper Scissors Rock
+//
+//  Created by Elina Mansurova on 2020-08-31.
+//  Copyright © 2020 Elina Mansurova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
